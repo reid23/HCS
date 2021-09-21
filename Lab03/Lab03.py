781e644f7228536c34e05028ddc85b328d447a42
reid23 <reid.dye@gmail.com>
2021-09-20 21:53:48 -0700

    commented lab03

--- Lab03/Lab03.py
+++ Lab03/Lab03.py
@@ -1,4 +1,3 @@
-#%%
 #imports
 import numpy as np
 from numpy import format_float_positional as ffp
@@ -7,17 +6,23 @@ import warnings
 from math import *
 import random
 
-#%%
+
 #utils for points()
+#description in readme
 tanAng=lambda a: degrees(atan(a[1]/a[0]))
 correctAng=lambda a: a if a>=0 else 360+a
 
-#%%
+
 def points(*points):
+    #intro line
+    print('This function calculates a bunch of stuff about the two points you input.')
+
     with warnings.catch_warnings(): #warnings?  what warnings?
         warnings.simplefilter("ignore") #if it doesn't stop the program, it doesn't matter lol
 
         print('Welcome to the equation center!')
+
+        #check if points were given as args
         if not not points:
             a=np.array(points[0])
             b=np.array(points[1])
@@ -27,33 +32,46 @@ def points(*points):
             print(f'Enter the coordinates of point A as x,y: {str(a)[1:-1]}')
             print(f'Enter the coordinates of point B as x,y: {str(b)[1:-1]}')
         else:
+            #get input if needed
             a = np.array(input('Enter the coordinates of point A as x,y: ').split(','))
             b = np.array(input('Enter the coordinates of point B as x,y: ').split(','))
 
-        #if the user's an awful person
+        #if the user forgets to give args
+        #use random inputs to screw with them instead of throwing a handy error
         if a==np.array(['']) or b==np.array(['']):
             a=[random.uniform(-1,1) for _ in [0,0]] #heyyy it's a little shorter
             b=[random.uniform(-1,1), random.uniform(-1,1)]
             print(f'Enter the coordinates of point A as x,y: {str(a)[1:-1]}')
             print(f'Enter the coordinates of point B as x,y: {str(b)[1:-1]}')
 
-        a, b = a.astype(np.float), b.astype('float')
-        #a = np.array([round(i, 2) for i in a])
-        #b = np.array([round(i, 2) for i in b])
 
+        #convert to floats
+        a, b = a.astype(np.float), b.astype('float')
+
+        #calculate slope
+        #we don't need to worry abt zeroDivisionError bc numpy is smrt
+        #when doing 1/0, numpy takes the limit as the denominator goes to 0
+        #instead of actually zero
+        #so it just raises a warning and returns inf, which is perfect
         slope=(a[1]-b[1])/(a[0]-b[0])
 
-        # turns out numpy doesn't throw a ZeroDivisionError, but rather 
-        # takes the limit as the denominator approaches zero, resulting in inf.
-        # but it does raise a RuntimeWarning, which is why there's the warnings.catch_warnings
+        #if slope is undefined
         if abs(slope)==inf:
             slope='undefined'
             equation=f'x={a[0]}'
+
+        #if line is horiz
         elif slope==0:
             equation=f'y={a[1]}'
-        elif sum(a==b)/2: #evaluating the truth of an array is ambiguous
+
+        # if points are the same
+        #have to do sum/2 to avoid 'The truth value of an array with more than one element is ambiguous.'
+        elif sum(a==b)/2:
             equation=f'ambiguous, not enough data.'
             slope='ambiguous, not enough data'
+        
+        #if the line isn't normal
+        #ie if the user isn't a hypothetical cs teacher trying to find corner cases
         else:
             c=ffp((-1*slope*a[0])+a[1], sign=True, precision=2, trim='k')
             slope=ffp(slope, precision=2, trim='k')
@@ -73,44 +91,60 @@ def points(*points):
         aAng= tanAng(a) if a[0]>0 else (tanAng(a))+180
         bAng= tanAng(b) if b[0]>0 else (tanAng(b))+180
 
+        #get rid of negative angles
         aAng=correctAng(aAng)
         bAng=correctAng(bAng)
 
-
+        #calc angle and round
         angle=bAng-aAng
         angle=ffp(angle, precision=2, trim='k')
+        #np.format_float_positional is just round() on steroids that returns a string.
+        #it's seriously great
+        #there's so many options!
 
+        #print output stuffs
         print('***************************')
         print(f'The slope of the line is {slope}')
         print(f'The distance between the points is {ffp(norm(a-b), precision=2)}')
         print(f'The equation of the line is {equation}')
         print(f'The angle of rotation about the origin from A to B is {angle} degrees')
 
-#%%
 def testScores(*scores):
+    #intro line
+    print('This function calculates a bunch of stats about your test scores')
+    #define all the letters
     #clunky but more line-efficient than a bunch of ifs
     scoreLetters = {0:'F', 1:'F', 2:'F', 3:'F', 4:'F', 5:'F', 6:'D', 7:'C', 8:'B', 9:'A', 10:'A'}
 
+    #check if scores were given as args
     if not not scores:
         scores=np.array(scores)  #tuples.  Nasty things.  Won't have 'em in the house.
         print('Welcome to the test center! How many tests are there?', len(scores))
+    
     else:
+        #get input if needed
         tests = input('Welcome to the test center!  How many tests are there? ')
         tests = int(tests)
 
+        #make sure it won't break
         assert tests > 0, 'Minimum input is 1 score.'
+
+        #init scores var
+        #appending is hard to type and computationally slow
         scores = np.zeros(tests)
+
+        #get input and put it in scores
         for counter, _ in enumerate(scores):
             score = int(input(f'Enter the score for test {counter + 1}: '))
             assert score >= 0, 'Negative scores are not allowed.'
             scores[counter]=score
     
+    #print outputs
     print('Your high score was', scores[np.argmax(scores)])
     print('Your low score was', scores[np.argmin(scores)])
     print('Your test average is', np.average(scores), '(This is in the', scoreLetters[np.average(scores)//10],'range).')
     print('The standard deviation of your scores is', np.std(scores))  #I find the naming of this function somewhat unfortunate
 
-#%%
 
 def leap(*year):
     # using *year so that it's optional
@@ -162,6 +196,7 @@ def leap(*year):
         if not i>=0:
             panic=True
 
+    #raise error if input is wrong
     assert panic==False, f'Invalid input, expected a positive integer but received {year}'
 
     #actually figure out if it's a leap year
@@ -176,18 +211,8 @@ def leap(*year):
         else:
             output[counter] = False
 
-
+    #print/return output
     if manual:
         print(f"{int(year[0])} {'is' if output[0] else 'is not'} a leap year.")
     else:
         return output
-# %%
-
-import time
-import random
-start=time.time()
-for i in range(10000):
-    points([1,1],[1,1])
-end=time.time()
-print('Time take:',end-start)
-# %%
