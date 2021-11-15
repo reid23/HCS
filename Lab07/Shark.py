0b300d86339c4929f290d8215b1e5153a1a9c9e8
reid23 <reid.dye@gmail.com>
2021-11-14 22:19:43 -0800

    almost there....

--- Lab07/Shark.py
+++ Lab07/Shark.py
@@ -1,4 +1,13 @@
 from graphics import *
 class Shark:
-    def __init__(self, img='Shark.png'):
-        self.img=Image(Point())
+    def __init__(self, imgPath='Shark___.png', pos=[7,2], draw=True):
+        self.imgPath=imgPath
+        self.pos=pos
+    def getGraphicsObjects(self):
+        return self.img
+    def getImgPath(self):
+        return self.imgPath
+    def getPos(self):
+        return self.pos
+    def setImage(self, image):
+        self.img=image.clone()
--- Lab07/SharkGUI.py
+++ Lab07/SharkGUI.py
@@ -1,102 +1,101 @@
 from graphics import *
-class Things:
-    #these are all index constants
-    SHARK = 0
-    FISHA = 1
-    FISHB = 2
-    FISHC = 3
-
-    START = 4
-    QUIT  = 5
-    MOVE  = 6
-
-    ENTRYA = 7
-    ENTRYB = 8
-    ENTRYC = 9
-
-    MSGBOX = 10
-class gui:
-    def __init__(self, shark, fisha, fishb, fishc, start, quit, move, entrya, entryb, entryc, msgbox, winTitle='SharkGUI', winSize=[800, 900], drawButtons=True, drawAnimals=True):
-        """constructor for gui
-
-        Args:
-            animalNames (list): a list of the names of the animals
-            animalObjects (list): the actual animal objects, in the same order as the name list
-            buttonNames (list): a list of the names of the buttons
-            buttonObjects (list): the actual button objects, in the same order as the name list
-            winTitle (str, optional): the title of the graphics window. Defaults to 'SharkGUI'.
-            winSize (list, optional): the size of the graphics window. Defaults to [800, 900].
-        """
-        self.animals=dict(zip(animalNames, animalObjects)) #store animals
-        self.buttons=dict(zip(buttonNames, buttonObjects)) #store buttons
+class Gui:
+    SHARK, FISHA, FISHB, FISHC, START, QUIT, MOVE, ENTRYA, ENTRYB, ENTRYC, MSGBOX = list(range(0, 11)) #index constants
+    NORTH, EAST, SOUTH, WEST = '000', '090', '180', '270'
+    def __init__(self, shark, fisha, fishb, fishc, start, quit, move, entrya, entryb, entryc, msgbox, winTitle='SharkGUI', winSize=[800, 900], drawButtons=True, drawAnimals=True, drawEntries=True, drawMsgBox=True):
+        self.objects=[shark, fisha, fishb, fishc, start, quit, move, entrya, entryb, entryc, msgbox]
         self.win=GraphWin(winTitle, winSize[0], winSize[1]) #make window
         self.win.setCoords(-0.5, -1.5, 9.5, 9.5) #fix it so that grid locations are integers
         self.grid=[Rectangle(Point(i-0.5, j-0.5), Point(i+0.5, j+0.5)) for i in range(10) for j in range(10)] #create grid
+        for animal in self.objects[0:5]: animal.setImage(Image(Point(*animal.getPos()), animal.getImgPath().replace('___', '000')))
         if drawButtons:
-            for _, button in self.buttons:
+            for button in self.objects[4:7]:
                 for obj in button.getGraphicsObjects(): obj.draw(self.win) #draw all the buttons
         if drawAnimals:
-            for _, animal in self.animals:
+            for animal in self.objects[0:5]:
                 for obj in animal.getGraphicsObjects(): obj.draw(self.win) #draw all the animals
+        if drawEntries:
+            for entry in self.objects[7:]:
+                for obj in entry.getGraphicsObjects(): obj.draw(self.win)
+        if drawMsgBox:
+            for obj in self.objects[10].getGraphicsObjects(): obj.draw(self.win)
+    def moveAnimal(self, animal, pos, rot, absolutePos=False):
+        self.undraw(animal)
+        self.objects[animal].setImage(Image(Point(*animal.getPos()), animal.getImgPath().replace('___', rot)))
+        self.draw(animal)
+        if absolutePos:
+            self.objects[animal].getPos()
+        for obj in self.objects[animal].getGraphicsObjects(): obj.move(*pos)
     def drawGrid(self):
         'draws the grid'
         for i in self.grid: i.draw(self.win)
     def undrawGrid(self):
         'undraws the grid'
         for i in self.grid: i.undraw(self.win)
-    
     @property
     def win(self): return self.win
-
     @win.setter
     def setWin(self, win):
+        """sets win to a window
+        Args:
+            win (GraphWin): the new window
+        """
         self.win.close()
         del(self.win)
         self.win=win
-    
-    def getButton(self, name): 
-        'returns the button object with the name [name].'
-        return self.buttons[name]
-    def setButton(self, name, button):
-        'adds a button'
-        self.buttons[name].undraw()
-        self.buttons[name]=button
-    def drawButton(self, name):
-        'draws a specific button'
-        for obj in self.buttons[name].getGraphicsObjects(): obj.draw(self.win)
-    def undrawButton(self, name): 
-        'undraws a specific button'
-        self.buttons[name].undraw()
-    def getAnimal(self, name): 
-        'returns the animal object with the name [name]'
-        return self.animals[name]
-    def drawAnimal(self, name): 
-        'draws a specific animal'
-        for obj in self.animals[name].getGraphicsObjects(): obj.draw(self.win)
-    def undrawAnimal(self, name): 
-        'undraws a specific animal'
-        self.animals[name].undraw()
+    def getThing(self, thing):
+        """returns any of the graphics objects
+        Args:
+            thing (gui constant): the thing to get, in the form Gui.FISHC
+        Returns:
+            animal, button, etc: the object
+        """
+        return self.objects[thing]
+    def setThing(self, thing, newObject):
+        """sets the [thing] to [newObject]
+        Args:
+            thing (Gui constant): the thing to set. In the form Gui.FISHC
+            newObject (button, animal, etc. object): the thing to set Gui.[whatever] to.
+        """
+        self.objects[thing]=newObject
+    def thingMethod(self, thing, func, eva=True, exe=True, *args, **kwargs):
+        try:
+            if exe:
+                exec(f'self.objects[{thing}].{func}(*{args}, **{kwargs})')
+            if eva:
+                return eval(f'self.objects[{thing}].{func}(*{args}, **{kwargs})')
+        except Exception as e:
+            print('Error occurred!', e)
+            return e
+    def draw(self, thing): 
+        """draws any of the graphics objects
+        Args:
+            thing (Gui constant): the thing to draw. In the form Gui.FISHC
+        """
+        for obj in self.objects[thing].getGraphicsObjects(): obj.draw(self.win)
+    def undraw(self, thing): 
+        """undraws any of the graphics objects
+        Args:
+            thing (Gui constant): the thing to undraw. In the form Gui.FISHC
+        """
+        self.objects[thing].undraw()
     def getMouse(self): 
         'just win.getMouse without having to use win'
         return self.win.getMouse()
-    def waitButtonClick(self, buttonName=None):
+    def waitButtonClick(self, button=None):
         """waits for a button click
-
         Args:
             buttonName (str, optional): the name of the button to monitor. if not given, the first button to be clicked is returned. Defaults to None.
-
         Returns:
             str: the name of the button that was clicked
         """
         while True: #main loop
             p=self.win.getMouse() #get mouse pos
-            if buttonName: #if buttoName was passed
-                if self.buttons[buttonName].clicked(p): #check if it was clicked correctly 
-                    return buttonName #for compatibility
+            if button: #if buttoName was passed
+                if self.buttons[button].clicked(p): #check if it was clicked correctly 
+                    return 'START' if button==4 else ('QUIT' if button==5 else 'MOVE')  #for compatibility
                 continue #don't do the other for loop
-            for name, button in self.buttons: 
-                if button.clicked(p): return name #check clicked state
-
-
-def foo(arg1, arg2, *args, **kwargs):
-    print(args)
+            for button in self.objects[4:7]: 
+                if button.clicked(p): 
+                    return 'START' if button==4 else ('QUIT' if button==5 else 'MOVE')
+                    
