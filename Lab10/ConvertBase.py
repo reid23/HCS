f106fadfa9a7e5c93b1ae3cf0aedc64675e9237b
Reid Dye <reid.dye@gmail.com>
2022-03-15 18:46:35 -0700

    oops havent commited in a while but it's all working and finalized in rust

--- Lab10/ConvertBase.py
+++ Lab10/ConvertBase.py
@@ -1,36 +1,30 @@
 from math import log, floor
 
-def cvtr(num: str|int, cur: int, end: int, place: int =-1):
+def toTen(num: str, cur: int): #for explanation of how the one in cvtr works
+    return sum([({
+        'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15
+            }[char] 
+            if char in 'abcdef' 
+            else int(char))*(cur**counter) 
+                for counter, char in enumerate(num[::-1])])
 
-    if cur != 10: #convert to base 10 on first recursion
-        newNum=[]
-        num=list(num)
-        for counter, i in enumerate(num):
-            if i in 'abcdef':
-                num[counter]=ord(i)-87
-            else:
-                num[counter]=int(i)
-        num = num[::-1]
-        for counter, i in enumerate(num):
-            newNum.append(i*(cur**counter))
-        num=sum(newNum)
-        cur=10
+def toStr(num): return str(num) if num<10 else chr(num-87)
 
-    if place==0:
-        return str(num)
-    if place==-1:
-        place = floor((log(num, end)))
+def toTenR(num: str, cur: int):
+    digit = ord(num[-1])-87 if num[-1] in 'abcdef' else int(num[-1])
+    return digit*(cur**(len(num)-1)) + toTenR(num[:-1], cur) if len(num)>1 else digit
+def cvtrBig(num: str|int, cur: int, end: int, place: int = -1):
+    if place == -1: 
+        num = toTenR(num[::-1], cur)
+        place = floor(log(num, end))
+    return toStr(num) if place==0 else toStr(num//(end**place)) + cvtrBig(num%(end**place), 10, end, place-1)
 
-    digit = num//(end**place)
-    remainder = num%(end**place)
-    return str(digit) + cvtr(remainder, cur, end, place-1)
 
-def error(reason):
-    print('\nError:', reason, '\n')
-
+def cvtr(num: str|int, cur: int, end: int, place: int =-1): return (lambda num: str(num) if num<10 else chr(num-87))((sum([(ord(char)-87 if char in 'abcdef' else int(char))*(cur**counter) for counter, char in enumerate(num[::-1])]) if isinstance(num, str) else num)) if (place if place != -1 else floor(log((sum([(ord(char)-87 if char in 'abcdef' else int(char))*(cur**counter) for counter, char in enumerate(num[::-1])]) if isinstance(num, str) else num), end)))==0 else (lambda num: str(num) if num<10 else chr(num-87))((sum([(ord(char)-87 if char in 'abcdef' else int(char))*(cur**counter) for counter, char in enumerate(num[::-1])]) if isinstance(num, str) else num)//(end**(place if place != -1 else floor(log((sum([(ord(char)-87 if char in 'abcdef' else int(char))*(cur**counter) for counter, char in enumerate(num[::-1])]) if isinstance(num, str) else num), end))))) + cvtr((sum([(ord(char)-87 if char in 'abcdef' else int(char))*(cur**counter) for counter, char in enumerate(num[::-1])]) if isinstance(num, str) else num)%(end**(place if place != -1 else floor(log((sum([(ord(char)-87 if char in 'abcdef' else int(char))*(cur**counter) for counter, char in enumerate(num[::-1])]) if isinstance(num, str) else num), end)))), 10, end, (place if place != -1 else floor(log((sum([(ord(char)-87 if char in 'abcdef' else int(char))*(cur**counter) for counter, char in enumerate(num[::-1])]) if isinstance(num, str) else num), end)))-1)
 
 def main():
     print('Welcome to the base conversion program! If you would like to quit this program, use C-d (like standard python shells).\n')
+    chars = '0123456789abcdef'
     while True:
         try:
             start = input('Source Base: ')
@@ -39,36 +33,29 @@ def main():
         except EOFError:
             while True:
                 inString = input('\nAre you sure you want to quit [y/n]? ')
-                if 'y' in inString: 
-                    exit()
-                elif 'n' in inString:
-                    break #go back to getting new numbers
-                else:
-                    pass
+                if 'y' in inString: exit()
+                elif 'n' in inString: break #go back to getting new numbers
+                else: pass
             continue
 
-        chars = '0123456789abcdef'
-
         try:
-            for i in start:
-                assert 48<=ord(i)<=57, f'Expected base 10 number for starting base but received {start}' # make sure all chars are digits (no decimals or letters)
-            for i in end:
-                assert 48<=ord(i)<=57, f'Expected base 10 number for destination base but received {end}' # same thing
-            start=int(start)
-            end=int(end)
+            for i in start: assert 48<=ord(i)<=57, f'Expected base 10 number for starting base but received {start}' # make sure all chars are digits (no decimals or letters)
+            for i in end: assert 48<=ord(i)<=57, f'Expected base 10 number for destination base but received {end}' # same thing
+            start, end=int(start), int(end) #convert str to int
 
             assert 2<=start<=16, f'Expected starting base in [2, 16] but received {start}'
             assert 2<=end<=16, f'Expected destination base in [2, 16] but received {end}'
 
-            num=num.lower()
-            numList=list(num)
+            numList=list(num.lower())
             for char in numList:
-                assert char in chars[:start], f'Expected positive integer source number in base {start} but received {num}'
+                assert char in chars[:start], f'Expected positive integer source number in base {start} but received {num.lower()}'
         except AssertionError as e:
-            error(e)
+            print('\nError:', e, '\n')
             continue
 
-        print(f'\nNEW NUMBER: {cvtr(num, start, end)}\n')
+        print(f'\nNEW NUMBER: {cvtr(num.lower(), start, end)}\n')
 
 
 if __name__ == '__main__': main()
+
+# %%
---
+++ Lab10/sorts/src/main.rs
@@ -0,0 +1,175 @@
+
+
+use rand::{Rng, thread_rng, distributions::Uniform};
+use std::time::Instant;
+
+fn insertion_sort(l: &mut Vec<f32>, _low: usize, _high: usize){
+    for i in 1..l.len(){
+        let mut j = i;
+        while j > 0{
+            if l[j - 1] > l[j] {
+                l.swap(j - 1, j);
+            }
+            j -= 1;
+        }
+    }
+}
+
+
+fn bubble_sort(l: &mut Vec<f32>, _low: usize, _high: usize){
+    let length = l.len();
+    for i in 0..length{
+        for j in 0..(length-(i+1)){ // +1 to prevent l[j+1] from being out of bounds
+            if l[j] > l[j+1]{
+                l.swap(j, j+1);
+            }
+        }
+    }
+}
+
+fn selection_sort(l: &mut Vec<f32>, _low: usize, _high: usize){
+    for j in 0..l.len(){
+        let mut minimum: f32 = 2.0;
+        let mut counter = 0;
+        let mut mindex = 0;
+        for i in &l[j..]{
+            if i <= &mut minimum{
+                mindex = counter;
+                minimum = *i; //derefernce bc i is borrowed
+            }
+            counter+=1;
+        }
+        l.swap(mindex, j)
+    }
+}
+
+
+fn partition(l: &mut Vec<f32>, low: usize, high: usize) -> usize{
+    let center: f32 = l[(high+low)/2] as f32;
+    let mut low = low-1;
+    let mut high = high+1;
+
+    loop {
+        low+=1;
+        while l[low]<center{
+            low += 1;
+        }
+        high-=1;
+        while l[high]>center{
+            high -= 1;
+        }
+        if low>=high{
+            return high;
+        }
+        l.swap(low, high);
+    }
+}
+
+fn quick_sort(l: &mut Vec<f32>, low: usize, high: usize){
+    if low < high{
+        let p=partition(l, low, high);
+        quick_sort(l, low, p);
+        quick_sort(l, p+1, high);
+    }
+}
+
+fn merge_sort(l: &mut Vec<f32>, _low: usize, _high: usize){
+    let m = merge_sort_main(l.clone());
+    for i in 0..m.len(){ //this can't be the best way....
+        l[i] = m[i];
+    }
+
+}
+
+fn merge_sort_main(l: Vec<f32>) -> Vec<f32>{
+    if l.len() <= 1{
+        return l;
+    }
+    let mid=l.len()/2;
+    let lin = l[0..mid].to_vec();
+    let rin = l[mid..].to_vec();
+    let (left, right) = (merge_sort_main(lin), merge_sort_main(rin));
+    return merge(&left, &right);
+}
+
+
+fn merge(l: &Vec<f32>, r: &Vec<f32>) -> Vec<f32> {
+    let mut merged: Vec<f32> = Vec::new();
+    let (mut lpos, mut rpos) = (0, 0);
+    let (llen, rlen) = (l.len(), r.len());
+    while lpos < llen && rpos < rlen {
+        if l[lpos] > r[rpos] {
+            merged.push(r[rpos]);
+            rpos += 1;
+        } else {
+            merged.push(l[lpos]);
+            lpos += 1;
+        }
+    }
+    if lpos == llen {
+        merged.extend(r[rpos..].to_vec());
+    } else {
+        merged.extend(l[lpos..].to_vec());
+    }
+
+    merged
+}
+
+
+fn time_sort<F: Fn(&mut Vec<f32>, usize, usize) -> ()>(name: String, sort: F, iters: u32, length: u32){ //low is passes as 1, high is len-1
+    let mut rng = thread_rng();
+    let range = Uniform::new(0.0, 1.0);
+    
+    let mut times:Vec<f64> = Vec::new();
+    
+    for _ in 0..iters{
+        let vals: Vec<f32> = (0..length).map(|_| rng.sample(&range)).collect();
+        let len = vals.len();
+        let start=Instant::now();
+        sort(&mut vals.clone(), 1, len-1);
+        let elapsed = start.elapsed().as_secs_f64();
+        times.push(elapsed);
+    }
+    println!("{:<18} {:<7} {:<10} {:<12} {:<12} {:<12}", name, iters, length, times.clone().into_iter().reduce(f64::min).unwrap(), times.clone().into_iter().reduce(f64::min).unwrap(), times.iter().sum::<f64>() / (times.len() as f64));
+    //println!("Method: {:<15} Sorts: {:<5} List Length: {:<10} Min: {:<12} Max: {:<12} Avg: {:<12}", name, iters, length, times.clone().into_iter().reduce(f64::min).unwrap(), times.clone().into_iter().reduce(f64::min).unwrap(), times.iter().sum::<f64>() / (times.len() as f64));
+}
+
+fn main() {
+    use std::env;
+
+    let args: Vec<String> = env::args().collect();
+    //println!("{:?}", args);
+    if args.len() >= 3{
+        let (iters, length) = (args[1].parse::<u32>().unwrap(), args[2].parse::<u32>().unwrap());
+        println!("From args:");
+        time_sort(String::from("quicksort"), quick_sort, iters, length);
+        time_sort(String::from("insertion sort"), insertion_sort, iters, length);
+        time_sort(String::from("selection sort"), selection_sort, iters, length);
+        time_sort(String::from("bubble sort"), bubble_sort, iters, length);
+        time_sort(String::from("merge sort"), merge_sort, iters, length);
+        println!("");
+    }
+    // let mut v: Vec<f32> = vec![9.0,7.0,5.0,5.0,4.0,3.0,7.0,9.0];
+    // merge_sort(&mut v, 1, 1);
+    // println!("sorted: {:?}", v);
+
+
+
+
+    println!("Method:            Sorts:  Length:    Min:         Max:         Avg:");
+    for i in 1..6{time_sort(String::from("Insertion Sort"), insertion_sort, 10, u32::pow(10, i));} println!("");
+    for i in 1..6{time_sort(String::from("Selection Sort"), selection_sort, 10, u32::pow(10, i));} println!("");
+    for i in 1..6{time_sort(String::from("Bubble Sort"), bubble_sort, 10, u32::pow(10, i));} println!("");
+    for i in 1..6{time_sort(String::from("Merge Sort"), merge_sort, 10, u32::pow(10, i));} println!("");
+    for i in 1..6{time_sort(String::from("Quicksort"), quick_sort, 10, u32::pow(10, i));} println!("");
+
+    // time_sort(String::from("Quicksort"), quick_sort, 10, 10);
+    // time_sort(String::from("Quicksort"), quick_sort, 10, 100);
+    // time_sort(String::from("Quicksort"), quick_sort, 10, 1_000);
+    // time_sort(String::from("Quicksort"), quick_sort, 10, 10_000);
+    // time_sort(String::from("Quicksort"), quick_sort, 10, 100_000);
+    // time_sort(String::from("Quicksort"), quick_sort, 10, 1_000_000);
+
+
+
+}
